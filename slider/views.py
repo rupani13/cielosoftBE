b975524188bf80447d9f2e0db90b58660ef9bb49
Himani13Bhardwaj <hrabhardwaj@gmail.com>
2022-04-16 23:48:28 +0530

    Update views.py

--- slider/views.py
+++ slider/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from sliders.models import Sliders
-from sliders.api.serializers import SliderSerializer
+from slider.models import Slider
+from slider.api.serializers import SliderSerializer
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
 # Create your views here.
 class SliderView(APIView):
     
-    model = Sliders
+    model = Slider
 
     def get(self, request):
-        slider_list = Sliders.objects.all().order_by('type_slider')
+        slider_list = Slider.objects.all().order_by('type_slider')
         serialzer = SliderSerializer(slider_list, context={"request": request}, many=True)
         return Response(serialzer.data)
