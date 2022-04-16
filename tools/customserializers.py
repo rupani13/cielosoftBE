3d36b325a4fee43811d450fcac43b92cb9cc818c
Himani13Bhardwaj <hrabhardwaj@gmail.com>
2022-04-16 23:21:17 +0530

    Add files via upload
    Uploaded tool module

---
+++ tools/customserializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from comment.api.serializers import CommentsSerializer
+from book.api.serializers import BooksSerializer
+from book.models import Books
+
+
+class BookLatestSerializer(BooksSerializer):
+    # upvote = serializers.CharField(source='book_details.upvote')
+    # downvote = serializers.CharField(source='book_details.downvote')
+    # comments = CommentsSerializer(many=True)
+    # author = serializers.CharField(source='author.author_name')
+
+    class Meta:
+        model = Books
+        fields = ['id', 'book_name', 'book_cover_url']
