05dfbe24c36a376423f16b809a03748148d369c3
Himani13Bhardwaj <hrabhardwaj@gmail.com>
2022-04-16 22:13:56 +0530

    Update serializers.py
    Updated Author serializers

---
+++ author/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from account.models import Account
+from author.models import Author
+from book.api.serializers import BooksSerializer
+
+
+class AuthorProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    books = BooksSerializer(many=True, read_only=True)
+    author_name = serializers.ReadOnlyField(source="account.name")
+    class Meta:
+        model = Author
+        fields = ['id', 'author_name', 'hobbies', 'profilepicture', 'books']
