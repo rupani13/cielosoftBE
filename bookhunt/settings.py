690eeb0924012b31f4833d284fab652c65f89b82
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-06-21 17:51:51 +0530

    Update settings.py

--- bookhunt/settings.py
+++ bookhunt/settings.py
@@ -179,6 +179,6 @@ API_KEY = config('API_KEY')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL= "/media/"
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
