4d84c68242c3387f0bf92b4754c6ad045b76d9e1
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-07-28 13:59:37 +0530

    Add files via upload

--- book/admin.py
+++ book/admin.py
@@ -3,8 +3,14 @@ from book.models import Books, BookDetails, Chapter
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'book_name', 'chapters')
+    list_display = ('id', 'book_name', 'chapters', 'genre', 'author', 'status', 'book_details')
+
+class BookDetailsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'view', 'upvote', 'downvote')
+
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chapter_no', 'chapter_name', 'book_id', 'coins')
 # Register your models here.
 admin.site.register(Books, BookAdmin)
-admin.site.register(BookDetails)
-admin.site.register(Chapter)
+admin.site.register(BookDetails, BookDetailsAdmin)
+admin.site.register(Chapter, ChapterAdmin)
