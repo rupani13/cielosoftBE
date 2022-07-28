289b67a3af42e83fcd995c59e463b5c01f0d4eed
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-07-28 14:00:00 +0530

    Add files via upload

--- author/admin.py
+++ author/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from author.models import Author
 
 # Register your models here.
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'account', 'book_count']
+
+admin.site.register(Author, AuthorAdmin)
