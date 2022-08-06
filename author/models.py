c9485c1cfa1ff5ecb75a7c572cf968e8f3ad21b9
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-08-06 12:22:41 +0530

    Update models.py

--- author/models.py
+++ author/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django_countries.fields import CountryField
 from account.models import Account
 # Create your models here.
+def author_cloud_upload(instance, file_name):
+    file_path = os.path.join('static/profile', file_name)
+    return file_path
 class Author(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     book_count              = models.IntegerField(default=0)
     app_id                  = models.BigIntegerField
-    profilepicture          = models.ImageField(upload_to = 'static/profile', default = '', blank=True, null=True)
+    profilepicture          = models.ImageField(upload_to=author_cloud_upload, default = '', blank=True, null=True)
     hobbies                 = models.TextField(blank=True, null=True)
     intro                   = models.TextField(blank=True, null=True)
     def __str__(self):
