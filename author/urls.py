54b54235c1a4d0dc9ef4f233b69afc3f64d8fb46
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-08-17 13:22:13 +0530

    Update urls.py

--- author/urls.py
+++ author/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from author.views import AuthorView, AuthorProfileView, WriterCreate
+from author.views import AuthorView, AuthorProfileView, WriterCreate, WriterBooks
 
 app_name = 'author'
 
@@ -8,4 +8,5 @@ urlpatterns = [
 	# path('createbook', AuthorBookCreate.as_view()),
 	path('profile/', AuthorProfileView.as_view()),
  	path('bewriter', WriterCreate.as_view()),
+	path('writerbooks', WriterBooks.as_view()),
 ]
