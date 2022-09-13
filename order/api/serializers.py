e5f5a8d903fe57446b44bd82682d368eac3ea1a1
Himani Bhardwaj <hrabhardwaj@gmail.com>
2022-09-13 21:07:14 +0530

    Add files via upload

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -1,1 +1,12 @@
 
+from rest_framework import serializers
+from order.models import PurchaseCoin
+
+
+# Comments
+# ------------------------------------------------
+class PurchaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PurchaseCoin
+        fields = '__all__'
